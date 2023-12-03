ad7c8a8ad323974f27b0597d81043ded0e594a1e
Maxim Zhvakin <faulpelz1993@gmail.com>
2023-12-03 20:42:16 +0300

    Initial commit

---
+++ cppl-homeworks_05_03/cppl-homeworks_05_03.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class functor {
+    int sum;
+    int count;
+
+public:
+    functor() :sum{ 0 }, count{ 0 } {};
+    void operator()(std::vector<int> vec) {
+        for (auto x : vec) {
+            sum += x;
+            if (x % 3 == 0) {
+                count++;
+            }
+        }
+    }
+    int get_sum() {
+        return sum;
+    }
+    int get_count() {
+        return count;
+    }
+};
+
+void print_v(int a) {
+    std::cout << a << " ";
+}
+
+int main() {
+    std::vector<int> vec{ 4, 1, 3, 6, 25, 54 };
+    functor func;
+    func(vec);
+    std::cout << "[IN]: ";
+    std::for_each(vec.begin(), vec.end(), print_v);
+
+    std::cout << "\n[OUT]: " << "get_sum() = " << func.get_sum() << std::endl;
+    std::cout << "[OUT]: " << "get_count() = " << func.get_count() << std::endl;
+    return 0;
+}
